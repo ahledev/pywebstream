34b6d1d7ee14d11a4ecedfc14e44baa311b1a7d7
ahledev <63564716+ahledev@users.noreply.github.com>
2020-12-27 11:43:12 +0100

    Add files via upload

---
+++ webstream/main.py
@@ -0,0 +1,5 @@
+from pyimagesearch.motion_detection import SingleMotionDetector
+from imutils.video import VideoStream
+from flask import Reponse
+from flask import Flask
+from flask import render_template
