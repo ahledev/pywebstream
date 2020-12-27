3b0882f3fab79920a28c655655637353a9a1213c
ahledev <63564716+ahledev@users.noreply.github.com>
2020-12-27 11:53:36 +0100

    Add files via upload

--- webstream/main.py
+++ webstream/main.py
@@ -1,5 +1,14 @@
-from pyimagesearch.motion_detection import SingleMotionDetector
 from imutils.video import VideoStream
 from flask import Reponse
 from flask import Flask
 from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+
+app = Flask(__name__)
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
